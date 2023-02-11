11b609cb68e4139c5a336854c7e9cefe1f985327
Ruud van Asseldonk <dev@veniogames.com>
2023-02-11 21:20:06 +0100

    Move Circuit types into a new module

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,10 @@
+-- Zerorack -- Experiments with circuit compilation and zk-snarks
+-- Copyright 2023 Ruud van Asseldonk
+
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- A copy of the License has been included in the root of the repository.
+
 {-# LANGUAGE ApplicativeDo #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE ViewPatterns #-}
@@ -12,73 +19,7 @@ import Data.List (intercalate)
 import qualified Data.IntMap.Strict as IntMap
 
 import Expr (Expr (..), ExprExt (..), VarNum (..), RegNum (..), Increment (inc))
-
-data Constraint
-  = ConstrainEq Expr Expr
-  deriving (Eq, Ord, Show)
-
-data Definition
-  = Define VarNum ExprExt
-  deriving (Eq, Ord, Show)
-
-data Circuit a = Circuit (VarNum -> (a, VarNum, [Definition], [Constraint]))
-
-instance Functor Circuit where
-  fmap :: (a -> b) -> Circuit a -> Circuit b
-  fmap f (Circuit g) = Circuit $ \n ->
-    let
-      (x, n', ds, cs) = g n
-    in
-      (f x, n', ds, cs)
-
-instance Applicative Circuit where
-  pure :: a -> Circuit a
-  pure x = Circuit $ \n -> (x, n, [], [])
-
-  (<*>) :: Circuit (a -> b) -> Circuit a -> Circuit b
-  (Circuit f) <*> (Circuit g) = Circuit $ \n ->
-    let
-      (vf, n',  dsf, csf) = f n
-      (vx, n'', dsg, csg) = g n'
-    in
-      (vf vx, n'', dsg <> dsf, csg <> csf)
-
-instance Monad Circuit where
-  (>>=) :: Circuit a -> (a -> Circuit b) -> Circuit b
-  (Circuit f) >>= g = Circuit $ \n ->
-    let
-      (x, n',  dsf, csf) = f n
-      Circuit g' = g x
-      (y, n'', dsg, csg) = g' n'
-    in
-      (y, n'', dsg <> dsf, csg <> csf)
-
--- Define a new input variable.
-newInput :: Circuit Expr
-newInput = Circuit $ \n -> (Var n, inc n, [], [])
-
-buildCircuit :: Circuit a -> (a, [Definition], [Constraint])
-buildCircuit (Circuit f) =
-  let
-    (x, _, ds, cs) = f $ VarNum 0
-  in
-    (x, ds, cs)
-
-assertEq :: Expr -> Expr -> Circuit ()
-assertEq x y = Circuit $ \n -> ((), n, [], [ConstrainEq x y])
-
--- Record the definition for a new variable as an extended expression.
-define :: ExprExt -> Circuit Expr
-define expr = Circuit $ \n -> (Var n, inc n, [Define n expr], [])
-
-idiv :: Expr -> Expr -> Circuit Expr
-idiv x y = define $ Div x y
-
-fieldInv :: Expr -> Circuit Expr
-fieldInv x = define $ Inv x
-
-bitAnd :: Expr -> Expr -> Circuit Expr
-bitAnd x y = define $ And x y
+import Circuit (Constraint (..), Circuit, assertEq, bitAnd, buildCircuit, fieldInv, idiv, newInput)
 
 -- A vector of field elements, represented sparsely
 -- as a map of index to value.
